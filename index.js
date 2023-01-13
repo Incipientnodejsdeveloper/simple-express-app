74c41a61a9d89d94c67463d6b73bb63ffc8a198a
bhoomil <Bhoomil.shah@incipientinfo.com>
2023-01-13 15:45:35 +0530

    Simple express app

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/working', (req, res, next)=> {
+    res.status(200).json({
+        message: "Don't worry, I'm working!"
+    });
+    return;
+});
+
+app.listen(5000, ()=>{
+    console.log("Server listen: 5000");
+})
