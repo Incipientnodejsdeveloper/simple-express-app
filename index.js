f8054fb2584b875ac68fe51f10100a89de5943da
Incipientnodejsdeveloper <nodejsdeveloper.incipientinfo@gmail.com>
2023-01-13 16:10:16 +0530

    Add new request

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 
+
+app.get('/working/', (req, res, next)=> {
+    res.setHeader('custom-header', 'powered-by-nodejs-developer');
+    res.status(200).json({
+        message: "Don't worry, I'm also /working/!"
+    });
+    return;
+});
+
 app.get('/working', (req, res, next)=> {
     res.status(200).json({
-        message: "Don't worry, I'm working!"
+        message: "Don't worry, I'm /working!"
     });
     return;
 });
